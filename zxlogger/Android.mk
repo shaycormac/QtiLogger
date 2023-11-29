66a5292d89f4563ea8318a203e66e33cb0179cd4
ShayFan <shay.fan@kalerm.com>
2023-11-29 14:57:18 +0800

    mk,rc一些文件的添加注释

--- zxlogger/Android.mk
+++ zxlogger/Android.mk
@@ -15,24 +15,30 @@ LOCAL_SRC_FILES:= \
 		qsee.cpp \
 		tz.cpp \
 
-
+# 模块的名称， 后期就可以通过 mmm zxlogger来单编
 LOCAL_MODULE:= zxlogger
-LOCAL_MODULE_TAGS := optional 
+# 无论是在user，eng版本，都可以编译
+LOCAL_MODULE_TAGS := optional
+# 将被rc执行的脚本，是在vendor/ 下面还是在 product下面呢？
 LOCAL_INIT_RC := zxlogger.rc
+# 表明这个必须在vendor 供应商下面的目录中
 LOCAL_PROPRIETARY_MODULE := true
-
+# 依赖的静态库
 LOCAL_STATIC_LIBRARIES += libcutils
+# 依赖的动态库
 LOCAL_SHARED_LIBRARIES := \
 			liblog\
 			libutils
-
+# 这是一个可以执行的二进制文件
 include $(BUILD_EXECUTABLE) 
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := modemmask
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_SRC_FILES := modemmask.cfg
+# 这个表示中转 临时文件生成的地方
 LOCAL_MODULE_CLASS := ETC
+# 后缀
 LOCAL_MODULE_SUFFIX := .cfg
 include $(BUILD_PREBUILT)
 
@@ -49,8 +55,10 @@ LOCAL_MODULE := logcp
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_SRC_FILES := logcp.sh
 LOCAL_MODULE_CLASS := ETC
+# 这个位置表示最终安装位置，在out/product/类型/vendor/bin中
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/vendor/bin
 LOCAL_MODULE_SUFFIX := .sh
+# 安装完后要对这个文件进行权限处理，方便后面可以执行
 LOCAL_POST_INSTALL_CMD := \
         chmod 6755 $(LOCAL_MODULE_PATH)/$(LOCAL_SRC_FILES)
 include $(BUILD_PREBUILT)
